963a335d71a4c023e08717c6488ad475126d1181
B0BL3T <148268146+B0BL3T@users.noreply.github.com>
2023-12-04 14:10:52 -0500

    Unsuccessful trpubleshooting of geting player to show on screen
    Edits to game.py including changing the spawn point for player. Unsuccessesful in getting it to work correctly

--- game files/Game.py
+++ game files/Game.py
@@ -14,13 +14,17 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 running = True
 
-player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT - player.rect.width)
+# Draw scene/background
+screen.fill(SPACE_BLACK)
+
+# Spawn in player
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        screen.fill(SPACE_BLACK)
+
 
         pygame.display.flip()
